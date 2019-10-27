427a12d15b838d9ea686bc34f49a808d4c934a3a
Fazlul <fkarif.cse@gmail.com>
2019-10-27 16:01:13 +0100

    onclicksuggestion funtion render

--- src/App.js
+++ src/App.js
@@ -6,12 +6,30 @@ class App extends Component {
     super(props);
 
     this.items = ["Zara", "John", "Alex", "Johanas"];
+    this.state = {
+      suggestions: []
+    };
+    this.onClickSuggestions = this.onClickSuggestions.bind(this);
+  }
+
+  onClickSuggestions(event) {
+    const value = event.target.value;
+    let suggestions = [];
+    if (value.length > 0) {
+      const regex = new RegExp(`^$(value)`, "i");
+      suggestions = this.items.sort().filter(v => regex.test(v));
+    }
+
+    this.setState({
+      suggestions: suggestions
+    });
   }
 
   render() {
+    console.log(this.state.suggestions);
     return (
       <div>
-        <input type="text" />
+        <input onChange={this.onClickSuggestions} type="text" />
         <ul>
           {this.items.map(item => (
             <li>{item}</li>
